11c9cd881feb81f1359db457f93c26cb0a06218e
Yanlin0408 <55082617+Yanlin0408@users.noreply.github.com>
2021-02-12 23:00:37 +0000

    Update httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -35,19 +35,40 @@ class HTTPResponse(object):
 class HTTPClient(object):
     #def get_host_port(self,url):
 
+    #easily retrieve host, port and path from a given url by this function
+    # so we can use this everytime we need to obtain host, port and path
+    def getHostPortPath(self,url):
+        host = urlparse(url).hostname
+        port = urlparse(url).port
+        path = urlparse(url).path
+        if port != None:
+            return host,port,path
+        else:
+            port = 80
+            return host,port,path
+
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        response_needed = data.split("\n")
+        code = int(response_needed[0].split(' ')[1])
+
+        return code
 
     def get_headers(self,data):
-        return None
+        return data.split("\n")[0]
 
     def get_body(self, data):
-        return None
+        if data:
+            try:
+                body = data.split('\r\n\r\n')[1]
+            except:
+                raise Exception('Body not Found!')
+
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,14 +89,42 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ""
+        #first, obtain host, port, and path information from given url
+        host,port,path = self.getHostPortPath(url)
+        #second, connect
+        self.connect(host,port)
+        #third, send data
+        parsed_url = urllib.parse.urlparse(url)
+        # concatenate the query to path
+        if (parsed_url.query != ''):
+            path += "?" + parsed_url.query
+        complete_command = ("GET {p1} HTTP/1.1\r\n"+"Host: {p2}\r\n"+"Accept: */*\r\n\r\n")
+        final_command = complete_command.format(p1=path,p2=host)
+        self.sendall(command)
+        #then receive data
+        recv_data = self.recvall(self.socket)
+        #get code and body content
+        code = self.get_code(recv_data)
+        body = self.get_body(recv_data)
+        #close the connection
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        #first, obtain host, port, and path information from given url
+        host,port,path = self.getHostPortPath(url)
+        self.connect(host,port)
+        if args:
+            self.sendall("POST {file} HTTP/1.1\r\n"  + "Host: {host}\r\n" + "Content-Length: {length2}\r\n"  + "Content-Type: {content_type}; charset=UTF-8\r\n\r\n")
+
         return HTTPResponse(code, body)
+
+        #https://github.com/UAACC/CMPUT404-assignment-web-client
+        #from Dongheng
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
